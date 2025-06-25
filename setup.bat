887ecb88b896f80b2df5576774950dbac1bf2f93
git-Oskar <belczewski.o@gmail.com>
2025-06-25 17:16:59 +0200

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,3 +1,12 @@
-if not exist build\Debug mkdir build\Debug
-g++ projekt.cpp -g -o build\Debug\outDebug.exe -lgdi32
-build\Debug\outDebug.exe
+REM Buduje projekt za pomocą CMake i uruchamia program
+
+if not exist build mkdir build
+cd build
+cmake ..
+cmake --build . --config Debug
+if %errorlevel% neq 0 (
+    echo Kompilacja nie powiodla sie!
+    exit /b %errorlevel%
+)
+cd Debug
+outDebug.exe
